7585f214c92dd1eacdffd9e3e48a25b325066ae6
Wilman Jose Vega Castilla <wilmanvega@gmail.com>
2012-10-08 19:54:44 -0500

    Habia un error aca

--- de.vogella.emf.webpage.usingmodel/src/de/vogella/emf/webpage/usingmodel/UsingEMFModel.java
+++ de.vogella.emf.webpage.usingmodel/src/de/vogella/emf/webpage/usingmodel/UsingEMFModel.java
@@ -1,5 +1,27 @@
 package de.vogella.emf.webpage.usingmodel;
 
+import de.vogella.emf.webpage.model.webpage.MyWeb;
+import de.vogella.emf.webpage.model.webpage.WebPage;
+import de.vogella.emf.webpage.model.webpage.WebpageFactory;
+import de.vogella.emf.webpage.model.webpage.impl.WebPageImpl;
+import de.vogella.emf.webpage.model.webpage.impl.WebpagePackageImpl;
+
 public class UsingEMFModel {
+	//WebpagePackageImpl.init();
+    // Retrieve the default factory singleton
+    WebpageFactory factory = WebpageFactory .eINSTANCE;
+    // Create an instance of myWeb
+    MyWeb myWeb = factory.createMyWeb();
+    
+    
+    
+    myWeb.setDescription("This is a description");
+    // Create a page
+    Webpage webpage = factory.createWebpage();
+    webpage.setTitle("This is a title");
+    // Add the page to myWeb
+    myWeb.getPages().add(webpage);
+    // and so on, and so on
+    // as you can see the EMF model can be (more or less) used as standard Java
 
 }
